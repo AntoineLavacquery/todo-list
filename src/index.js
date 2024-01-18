4215864da5aacf0adb0d404ae7d0c01c4f8deb04
Antoine LAVACQUERY <antoine.lavacquery@proton.me>
2024-01-19 00:11:09 +0100

    Code addTodoButton directly in DOM

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import { compareAsc, format } from "date-fns";
 import Storage from "./modules/storage";
-import * as elements from './modules/elements.js';
 
 class Todo {
     constructor(title, project, dueDate, priority) {
@@ -24,13 +23,14 @@ storage.addTodo(todo2);
 storage.addTodo(todo3);
 storage.addTodo(todo4);
 
-const todoContainer = document.querySelector("div#main");
+const todoContainer = document.querySelector("div#todo-container");
+const addTodoButton = document.querySelector("button#add-todo");
 
 function displayInbox() {
     const todos = storage.getAllTodos();
     todoContainer.innerHTML = "";
     todos.forEach((todo) => {
-        todoContainer.appendChild(elements.createTodoElement(todo));
+        todoContainer.appendChild(createTodoElement(todo));
     });
 }
 
@@ -38,7 +38,7 @@ function displayToday() {
     const todos = storage.getTodaysTodos();
     todoContainer.innerHTML = "";
     todos.forEach((todo) => {
-        todoContainer.appendChild(elements.createTodoElement(todo));
+        todoContainer.appendChild(createTodoElement(todo));
     });
 }
 
@@ -46,7 +46,7 @@ function displayThisWeek() {
     const todos = storage.getThisWeekTodos();
     todoContainer.innerHTML = "";
     todos.forEach((todo) => {
-        todoContainer.appendChild(elements.createTodoElement(todo));
+        todoContainer.appendChild(createTodoElement(todo));
     });
 }
 
@@ -57,23 +57,84 @@ function displayProject(project) {
     }
     todoContainer.innerHTML = "";
     todos.forEach((todo) => {
-        todoContainer.appendChild(elements.createTodoElement(todo));
+        todoContainer.appendChild(createTodoElement(todo));
     });
-}
-
-function appendAddTodoButton() {
-    todoContainer.appendChild(elements.createAddTodoButton());
 }
 
 function loadProjects() {
     const projectsNames = storage.getProjectsNames();
     projectsNames.forEach((projectName) => {
-        elements.createButton(projectName, "ul#project", displayProject);
+        createButton(projectName, "ul#project", displayProject);
     });
 }
 
-elements.createButton("Inbox", "ul#home", displayInbox);
-elements.createButton("Today", "ul#home", displayToday);
-elements.createButton("This Week", "ul#home", displayThisWeek);
+function createButton(label, containerQuery, displayFunction) {
+    const button = document.createElement("button");
+    button.type = "button";
+    button.classList.add("nav-link", "link-body-emphasis", "btn", "btn-link");
+    button.textContent = label;
+    button.addEventListener("click", function () {
+        desactivateAllButtons();
+        button.classList.add("active");
+        if (containerQuery === "ul#project") {
+            addTodoButton.classList.remove("disabled");
+            displayFunction(label);
+        } else {
+            addTodoButton.classList.add("disabled");
+            displayFunction(label);
+        }
+    });
+    const li = document.createElement("li");
+    li.appendChild(button);
+    document.querySelector(containerQuery).appendChild(li);
+}
+
+function createTodoElement(todo) {
+    const todoElement = document.createElement("div");
+    todoElement.classList =
+        "alert alert-secondary alert-dismissible fade show d-flex justify-content-between";
+
+    const leftPart = document.createElement("div");
+    leftPart.classList = "d-flex gap-2";
+
+    const checkbox = document.createElement("input");
+    checkbox.id = "checkbox";
+    checkbox.classList = "form-check-input";
+    checkbox.type = "checkbox";
+
+    const title = document.createElement("span");
+    title.id = title;
+    title.innerText = todo.title;
+
+    leftPart.appendChild(checkbox);
+    leftPart.appendChild(title);
+
+    const timeRemaining = document.createElement("div");
+    timeRemaining.id = "remaining";
+    timeRemaining.innerHTML = todo.dueDate;
+
+    const closeButton = document.createElement("button");
+    closeButton.type = "button";
+    closeButton.className = "btn-close";
+    closeButton.setAttribute("data-bs-dismiss", "alert");
+    closeButton.setAttribute("aria-label", "Close");
+
+    todoElement.appendChild(leftPart);
+    todoElement.appendChild(timeRemaining);
+    todoElement.appendChild(closeButton);
+
+    return todoElement;
+}
+
+function desactivateAllButtons() {
+    const homeButtons = document.querySelectorAll("ul#home button");
+    const projectButtons = document.querySelectorAll("ul#project button");
+    const buttons = [...homeButtons, ...projectButtons];
+    buttons.forEach((button) => button.classList.remove("active"));
+}
+
+createButton("Inbox", "ul#home", displayInbox);
+createButton("Today", "ul#home", displayToday);
+createButton("This Week", "ul#home", displayThisWeek);
 
 loadProjects();
